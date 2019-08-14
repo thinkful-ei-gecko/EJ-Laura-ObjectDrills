d4bc251b28d61d511d454c9ef1c21e38dde7cfa7
EJ <ejg132@gmail.com>
2019-08-14 11:35:25 -0700

    studentReport

--- ObjectDrills.js
+++ ObjectDrills.js
@@ -31,33 +31,62 @@ Robert.sayHello();
 // let pickle = {} ;
 // console.log(updateObject(pickle));
 
-function personMaker() {
-  let person = {
-    firstName: 'Paul',
-    lastName: 'Jones',
-    fullName() {
-      console.log(`${this.firstName} ${this.lastName}`);
-    },
-  };
-  return person;
-}
+// function personMaker() {
+//   let person = {
+//     firstName: 'Paul',
+//     lastName: 'Jones',
+//     fullName() {
+//       console.log(`${this.firstName} ${this.lastName}`);
+//     },
+//   };
+//   return person;
+// }
 
-let character = personMaker();
-console.log(personMaker());
-character.fullName();
+// let character = personMaker();
+// console.log(personMaker());
+// character.fullName();
 
-const sampleObj = {
-    foo: 'foo',
-    bar: 'bar',
-    bizz: 'bizz',
-    bang: 'bang',
-  };
-  
-  function keyDeleter(obj) {
-    delete obj.foo;
-    delete obj.bar;
-    return obj;
-  }
+// const sampleObj = {
+//     foo: 'foo',
+//     bar: 'bar',
+//     bizz: 'bizz',
+//     bang: 'bang',
+//   };
 
-  keyDeleter(sampleObj);
-  console.log(sampleObj);
+//   function keyDeleter(obj) {
+//     delete obj.foo;
+//     delete obj.bar;
+//     return obj;
+//   }
+
+//   keyDeleter(sampleObj);
+//   console.log(sampleObj);
+
+// function makeStudentReport(data) {
+
+//   let array = [];
+
+//   data.forEach(element => {
+//     array.push(
+//       `${element.name}: ${element.grade}`
+//     )
+//   });
+
+//   return array;
+// }
+
+// let data = [{
+//   name: 'Jane Doe',
+//   grade: 'A'
+// },
+// {
+//   name: 'John Dough',
+//   grade: 'B'
+// },
+// {
+//   name: 'Jill Do',
+//   grade: 'A'
+// }
+// ];
+
+// console.log(makeStudentReport(data));

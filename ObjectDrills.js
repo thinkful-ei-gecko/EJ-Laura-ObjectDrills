fc26e155d202e4169962dae09ae5e18c5833a31b
laura <laura.j.elias@gmail.com>
2019-08-14 11:36:07 -0500

    object updater

--- ObjectDrills.js
+++ ObjectDrills.js
@@ -1,17 +1,32 @@
 'use strict';
 
+/*
 function createMyObject() {
-    let person = {
-        foo: 'bar',
-        answerToUniverse: 42,
-        "olly olly": 'oxen free',
-        sayHello: function () {
-            console.log('Hello!')
-        }
+  let person = {
+    foo: 'bar',
+    answerToUniverse: 42,
+    "olly olly": 'oxen free',
+    sayHello: function () {
+  console.log('Hello!')
     }
-    return person;
+  }
+  return person;
 }
 
 let Robert = createMyObject();
 
 Robert.sayHello();
+*/
+
+function updateObject(obj) {
+
+    obj.foo = "foo";
+    obj.bar = "bar";
+    obj.bizz = "bizz";
+    obj.bang = "bang";
+
+    return obj;
+}
+
+let pickle = {} ;
+console.log(updateObject(pickle));
